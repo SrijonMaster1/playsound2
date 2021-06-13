7e16513c687c0dad4c644fd348bc128f966fb2cc
Kethan Vegunta <kethan@vegunta.com>
2021-06-13 11:27:54 -0700

    update code and Readme

--- playsound.py
+++ playsound.py
@@ -1,12 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import time
+
+
 class PlaysoundException(Exception):
     pass
 
-def _playsoundWin(sound, block = True):
+
+def _playsoundWin(sound, block=True):
     '''
-    Utilizes windll.winmm. Tested and known to work with MP3 and WAVE on
-    Windows 7 with Python 2.7. Probably works with more file formats.
-    Probably works on Windows XP thru Windows 10. Probably works with all
-    versions of Python.
+    Utilizes windll.winmm. Tested and known to work with MP3 and Wave on
+    Windows 10 with Python 3.8 and 3.9. Probably works with more file formats.
 
     Inspired by (but not copied from) Michael Gundlach <gundlach@gmail.com>'s mp3play:
     https://github.com/michaelgundlach/mp3play
@@ -15,12 +21,10 @@ def _playsoundWin(sound, block = True):
     '''
     from ctypes import c_buffer, windll
     from random import random
-    from time   import sleep
-    from sys    import getfilesystemencoding
 
     def winCommand(*command):
         buf = c_buffer(255)
-        command = ' '.join(command).encode(getfilesystemencoding())
+        command = ' '.join(command).encode(sys.getfilesystemencoding())
         errorCode = int(windll.winmm.mciSendStringA(command, buf, 254, 0))
         if errorCode:
             errorBuffer = c_buffer(255)
@@ -36,14 +40,14 @@ def _playsoundWin(sound, block = True):
     winCommand('set', alias, 'time format milliseconds')
     durationInMS = winCommand('status', alias, 'length')
     winCommand('play', alias, 'from 0 to', durationInMS.decode())
-    
-    if block:
-        sleep(float(durationInMS) / 1000.0)
-        
-    winCommand('close', alias)
-    
 
-def _playsoundOSX(sound, block = True):
+    if block:
+        time.sleep(float(durationInMS) / 1000.0)
+
+    winCommand('close', alias)
+
+
+def _playsoundOSX(sound, block=True):
     '''
     Utilizes AppKit.NSSound. Tested and known to work with MP3 and WAVE on
     OS X 10.11 with Python 2.7. Probably works with anything QuickTime supports.
@@ -55,23 +59,22 @@ def _playsoundOSX(sound, block = True):
 
     I never would have tried using AppKit.NSSound without seeing his code.
     '''
-    from AppKit     import NSSound
+    from AppKit import NSSound
     from Foundation import NSURL
-    from time       import sleep
 
     if '://' not in sound:
         if not sound.startswith('/'):
-            from os import getcwd
-            sound = getcwd() + '/' + sound
+            sound = os.getcwd() + '/' + sound
         sound = 'file://' + sound
-    url   = NSURL.URLWithString_(sound)
+    url = NSURL.URLWithString_(sound)
     nssound = NSSound.alloc().initWithContentsOfURL_byReference_(url, True)
     if not nssound:
         raise IOError('Unable to load sound named: ' + sound)
     nssound.play()
 
     if block:
-        sleep(nssound.duration())
+        time.sleep(nssound.duration())
+
 
 def _playsoundNix(sound, block=True):
     """Play a sound using GStreamer.
@@ -84,12 +87,7 @@ def _playsoundNix(sound, block=True):
             "block=False cannot be used on this platform yet")
 
     # pathname2url escapes non-URL-safe characters
-    import os
-    try:
-        from urllib.request import pathname2url
-    except ImportError:
-        # python 2
-        from urllib import pathname2url
+    from urllib.request import pathname2url
 
     import gi
     gi.require_version('Gst', '1.0')
@@ -98,10 +96,8 @@ def _playsoundNix(sound, block=True):
     Gst.init(None)
 
     playbin = Gst.ElementFactory.make('playbin', 'playbin')
-    if sound.startswith(('http://', 'https://')):
-        playbin.props.uri = sound
-    else:
-        playbin.props.uri = 'file://' + pathname2url(os.path.abspath(sound))
+    playbin.props.uri = sound if sound.startswith(('http://', 'https://')) \
+        else 'file://' + pathname2url(os.path.abspath(sound))
 
     set_result = playbin.set_state(Gst.State.PLAYING)
     if set_result != Gst.StateChangeReturn.ASYNC:
@@ -115,14 +111,9 @@ def _playsoundNix(sound, block=True):
     playbin.set_state(Gst.State.NULL)
 
 
-from platform import system
-system = system()
-
-if system == 'Windows':
+if sys.platform == 'win32':
     playsound = _playsoundWin
-elif system == 'Darwin':
+elif sys.platform == 'darwin':
     playsound = _playsoundOSX
 else:
     playsound = _playsoundNix
-
-del system
---
+++ tests/test_sounds.py
@@ -0,0 +1,8 @@
+import sys
+import pathlib
+
+sys.path.insert(0, str(pathlib.Path(__file__).parents[1].absolute()))
+
+from playsound import playsound
+
+
